ecc227c290295d3e797f062b8bc896414952a089
Alice Teilhard <alice.teilhard@gmail.com>
2022-11-22 13:32:47 +0000

    changed index

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_121703) do
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
