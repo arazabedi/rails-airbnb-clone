7c8769ad8d4b841c18a61ca9068b9e117bde6ee3
Sloane Carroll <sloanecarroll15@gmail.com>
2022-11-22 11:35:28 +0000

    updated navbar

--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,34 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_151209) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "listings", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.decimal "price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.decimal "price"
+    t.bigint "listing_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listing_id"], name: "index_rentals_on_listing_id"
+    t.index ["user_id"], name: "index_rentals_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_151209) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "listings", "users"
+  add_foreign_key "rentals", "listings"
+  add_foreign_key "rentals", "users"
 end
