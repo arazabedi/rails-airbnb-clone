0965c076eaf67281ddb65e166a9969acca1ed4ff
Araz Abedi <arazabedi@hotmail.com>
2022-11-22 17:53:41 +0000

    Contact Us

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "listings#index"
   resources :listings, :users, :rentals
+  get "contact_us", to: "pages#contact_us"
   # get "listings/:id", to "listings#show", as: :listing
 end
