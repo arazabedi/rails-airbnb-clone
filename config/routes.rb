f122ebdd56dd5053d58891c9cef9f2b46a2b4e9d
Alice Teilhard <alice.teilhard@gmail.com>
2022-11-22 16:15:18 +0000

    day two show and index working

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "listings#index"
   resources :listings, :users, :rentals
+  # get "listings/:id", to "listings#show", as: :listing
 end
