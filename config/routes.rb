e4ae787da8be1fabca6085216598dffff98fa1b6
Araz Abedi <arazabedi@hotmail.com>
2022-11-22 16:23:51 +0000

    14.23

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/name'
-  get 'users/email'
 
-resources :listings, :users, :rentals
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-<<<<<<< HEAD
-
-
-=======
-  resources :user, :listing, :listing
->>>>>>> devise
+  resources :listings, :users, :rentals
 end
