6a4be9cbe0013bf948118c849b80d3190cf5a809
Alice Teilhard <alice.teilhard@gmail.com>
2022-11-22 18:58:00 +0000

    created  the possible link to html for API and still need to link upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,13 @@
 require 'faker'
 
 user1 = User.create(email: "araz@tess.com", password: "1234567")
-100.times do
+50.times do
   Listing.create!(user:user1, name: "#{Faker::Creature::Dog.name}", breed: "#{Faker::Creature::Dog.breed}", description: "#{Faker::Creature::Dog.meme_phrase}", price: "#{Faker::Number.decimal(l_digits: 3, r_digits: 2)}")
 end
+
+require "open-uri"
+
+file = URI.open("https://dog.ceo/api/breeds/image/random/50")
+listing = Listing.new(title: "NES", body: "A great console")
+listing.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+listing.save
