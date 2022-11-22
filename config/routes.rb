6cd386372d03bb14d9ccc64830a58daa87ffc912
Araz Abedi <arazabedi@hotmail.com>
2022-11-22 18:13:43 +0000

    day 2 final

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "listings#index"
   resources :listings, :users, :rentals
   get "contact_us", to: "pages#contact_us"
+  get "about", to: "pages#about"
   # get "listings/:id", to "listings#show", as: :listing
 end
