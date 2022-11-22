f0441282f552df5db628115451ee78155f3befcd
Alice Teilhard <alice.teilhard@gmail.com>
2022-11-22 14:45:37 +0000

    just finished the index html styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-require 'Faker'
+require 'faker'
 
 user1 = User.create(email: "araz@tess.com", password: "1234567")
 100.times do
